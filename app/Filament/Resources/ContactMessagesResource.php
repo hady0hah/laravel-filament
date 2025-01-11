a4be507e3d15983727bce85912f516af128d8a67
hady43 <hadi.hawy@layoutinternational.com>
2025-01-11 11:58:30 +0200

    Added Contact Messages form, list and model fields

--- app/Filament/Resources/ContactMessagesResource.php
+++ app/Filament/Resources/ContactMessagesResource.php
@@ -23,7 +23,31 @@ class ContactMessagesResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('full_name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('phone_number')
+                    ->required()
+                    ->maxLength(15)
+                    ->tel(),
+
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->email()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('subject')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\Textarea::make('message')
+                    ->required()
+                    ->maxLength(5000),
+
+                Forms\Components\Toggle::make('published')
+                    ->label('Published')
+                    ->default(true),
             ]);
     }
 
@@ -31,10 +55,61 @@ class ContactMessagesResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('full_name'),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->label('Phone Number'),
+                Tables\Columns\BooleanColumn::make('published')
+                    ->label('Published')
+                    ->trueIcon('heroicon-o-check')
+                    ->falseIcon('heroicon-o-x-mark'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('subject')->limit(20),
+//                Tables\Columns\TextColumn::make('message')->limit(20),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->date(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated At')
+                    ->date(),
             ])
             ->filters([
-                //
+                Tables\Filters\Filter::make('name')
+                    ->form([
+                        Forms\Components\TextInput::make('name')
+                            ->placeholder('Search by Name')
+                            ->label('Name'),
+                    ])
+                    ->query(function (Builder $query, array $data) {
+                        if (!empty($data['name'])) {
+                            $query->where('name', 'like', '%' . $data['name'] . '%');
+                        }
+                    }),
+                Tables\Filters\Filter::make('email')
+                    ->form([
+                        Forms\Components\TextInput::make('email')
+                            ->placeholder('Search by Email')
+                            ->label('Email'),
+                    ])
+                    ->query(function (Builder $query, array $data) {
+                        if (!empty($data['email'])) {
+                            $query->where('email', 'like', '%' . $data['email'] . '%');
+                        }
+                    }),
+                Tables\Filters\Filter::make('phone_number')
+                    ->form([
+                        Forms\Components\TextInput::make('phone_number')
+                            ->placeholder('Search by Phone')
+                            ->label('Phone'),
+                    ])
+                    ->query(function (Builder $query, array $data) {
+                        if (!empty($data['phone_number'])) {
+                            $query->where('phone_number', 'like', '%' . $data['phone_number'] . '%');
+                        }
+                    }),
+                Tables\Filters\Filter::make('published')
+                    ->query(fn (Builder $query): Builder => $query->where('published', true)),
+                Tables\Filters\Filter::make('unpublished')
+                    ->query(fn (Builder $query): Builder => $query->where('published', false)),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
--- app/Models/ContactMessages.php
+++ app/Models/ContactMessages.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ContactMessages extends Model
+class ContactMessages extends BaseModel
 {
     use HasFactory;
+
+
+    protected $table = 'contact_messages';
+
+    protected $fillable = [
+        'full_name',
+        'phone_number',
+        'email',
+        'subject',
+        'message',
+    ];
 }
