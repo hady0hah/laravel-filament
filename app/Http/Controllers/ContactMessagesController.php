5e6baafb2f3738243a01e59bd7c6406d94b2bd10
hady43 <hadi.hawy@layoutinternational.com>
2025-01-11 11:25:19 +0200

    Added basic Contact Messages Model Stuff

---
+++ app/Http/Controllers/ContactMessagesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactMessages;
+use Illuminate\Http\Request;
+
+class ContactMessagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ContactMessages $contactMessages)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ContactMessages $contactMessages)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ContactMessages $contactMessages)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ContactMessages $contactMessages)
+    {
+        //
+    }
+}
