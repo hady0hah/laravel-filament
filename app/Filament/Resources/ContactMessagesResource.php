d5d56daad801360a60e78ef688551d559eb1916c
hady43 <hadi.hawy@layoutinternational.com>
2025-01-11 11:25:12 +0200

    Added basic Contact Messages Resources Stuff

---
+++ app/Filament/Resources/ContactMessagesResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactMessagesResource\Pages;
+use App\Filament\Resources\ContactMessagesResource\RelationManagers;
+use App\Models\ContactMessages;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactMessagesResource extends Resource
+{
+    protected static ?string $model = ContactMessages::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactMessages::route('/'),
+            'create' => Pages\CreateContactMessages::route('/create'),
+            'edit' => Pages\EditContactMessages::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ContactMessagesResource/Pages/EditContactMessages.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ContactMessagesResource\Pages;
+
+use App\Filament\Resources\ContactMessagesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContactMessages extends EditRecord
+{
+    protected static string $resource = ContactMessagesResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
