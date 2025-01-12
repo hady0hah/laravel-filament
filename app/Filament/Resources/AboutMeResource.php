5888ee1310312511d08d4fae41236cc5fc005540
hady43 <hadi.hawy@layoutinternational.com>
2025-01-12 18:19:58 +0200

    Created Resources for about me Stuff

---
+++ app/Filament/Resources/AboutMeResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AboutMeResource\Pages;
+use App\Filament\Resources\AboutMeResource\RelationManagers;
+use App\Models\AboutMe;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AboutMeResource extends Resource
+{
+    protected static ?string $model = AboutMe::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAboutMes::route('/'),
+            'create' => Pages\CreateAboutMe::route('/create'),
+            'edit' => Pages\EditAboutMe::route('/{record}/edit'),
+        ];
+    }
+}
