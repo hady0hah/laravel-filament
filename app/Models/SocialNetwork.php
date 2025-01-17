3c24be1f51f5cf10a0e719ca21a9677deabbda5e
hady43 <hadi.hawy@layoutinternational.com>
2025-01-17 19:15:41 +0200

    Added Social Network Model Stuff

---
+++ app/Models/SocialNetwork.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SocialNetwork extends Model
+{
+    use HasFactory;
+
+    protected $table = 'social_networks';
+
+    protected $fillable = [
+        'title',
+        'link',
+        'image_path',
+        'published',
+    ];
+}
