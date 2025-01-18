6f19568a2daaa34f714ee2a1d7073e5ce35cfb92
hady43 <hadi.hawy@layoutinternational.com>
2025-01-18 11:08:46 +0200

    Added fields to table in migrations

---
+++ database/migrations/2025_01_18_085359_alter_table_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('link')->nullable();
+            $table->string('image_path');
+            $table->integer('show_in_homepage')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['new_field_1', 'new_field_2']);
+
+        });
+    }
+};
